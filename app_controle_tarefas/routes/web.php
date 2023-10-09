ef2037dc7a028bd584717978e2ee4003a4d9310e
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-10-09 15:09:11 -0300

    Update: Implementando o middleware auth

--- app_controle_tarefas/routes/web.php
+++ app_controle_tarefas/routes/web.php
@@ -24,3 +24,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('tarefa', 'App\Http\Controllers\TarefaController');//->middleware('auth');
