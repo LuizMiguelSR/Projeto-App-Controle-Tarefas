884e35de73183b852c8c2b97d8865cca7d76050c
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-11-10 09:18:41 -0300

    Update: Enviando um e-mail de cadastro de nova tarefa e exibindo os dados da tarefa

--- app_controle_tarefas/app/Http/Controllers/TarefaController.php
+++ app_controle_tarefas/app/Http/Controllers/TarefaController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Tarefa;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Mail;
+use App\Mail\NovaTarefaMail;
 
 class TarefaController extends Controller
 {
@@ -69,6 +71,8 @@ class TarefaController extends Controller
     public function store(Request $request)
     {
         $tarefa = Tarefa::create($request->all());
+        $destinatario = auth()->user()->email;
+        Mail::to($destinatario)->send(new NovaTarefaMail($tarefa));
         return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
     }
 
@@ -80,7 +84,7 @@ class TarefaController extends Controller
      */
     public function show(Tarefa $tarefa)
     {
-        dd($tarefa->getAttributes());
+        return view('tarefa.show', ['tarefa' => $tarefa]);
     }
 
     /**
