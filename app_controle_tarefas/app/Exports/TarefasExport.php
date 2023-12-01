3de3e28f8b9998f37ab015bd22e0b8e0140060a4
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-12-01 16:54:52 -0300

    Update: Definindo títulos na exportação

--- app_controle_tarefas/app/Exports/TarefasExport.php
+++ app_controle_tarefas/app/Exports/TarefasExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TarefasExport implements FromCollection
+class TarefasExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,5 +15,17 @@ class TarefasExport implements FromCollection
     {
         //return Tarefa::all();
         return auth()->user()->tarefas()->get();
+    }
+
+    public function headings():array //declarando o tipo de retorno
+    {
+        return [
+            'ID da Tarefa',
+            'ID do Usuário',
+            'Tarefa',
+            'Data limite conclusão',
+            'Data criação',
+            'Data atualização'
+        ];
     }
 }
